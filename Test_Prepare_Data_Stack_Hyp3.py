fbaf1d92b83aea5807cd9f0e60d71b861dcdb4fb
Alex-Lewandowski <aflewandowski@alaska.edu>
2020-06-15 14:37:31 -0800

    fix convert_to_subprocess, add doc strs and type hints

---
+++ Test_Prepare_Data_Stack_Hyp3.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+
+from getpass import getpass
+from asf_jupyter_test import ASFNotebookTest
+
+username = input("Earthdata Username: ")
+password = getpass("Earthdata Password: ")
+
+pth = "/home/jovyan/notebooks/SAR_Training/English/Hazards/Prepare_Data_Stack_Hyp3.ipynb"
+test = ASFNotebookTest(pth)
+
+######### REPLACE CELLS ###########
+
+test.replace_cell("login = EarthdataLogin()",
+                  f"login = EarthdataLogin(username='{username}', password='{password}')")
+
+data_dir = """
+data_dir = "NOTEBOOK_TESTING"
+try:
+    shutil.rmtree(data_dir)
+except FileNotFoundError:
+    pass
+os.mkdir(data_dir)
+"""
+test.replace_cell("data_dir = input_path", data_dir)
+
+test.replace_line("group_id = input", "group_id = input", "    group_id = ''\n")
+
+download_urls = """
+download_urls = ['https://hyp3-download.asf.alaska.edu/asf/data/S1A_IW_GRDH_1SDV_20170223T120417_20170223T120442_015411_0194AE_F18E-POEORB-30m-power-rtc-gamma.zip', 
+                 'https://hyp3-download.asf.alaska.edu/asf/data/S1A_IW_GRDH_1SDV_20170223T120442_20170223T120507_015411_0194AE_AFD2-POEORB-30m-power-rtc-gamma.zip', 
+                 'https://hyp3-download.asf.alaska.edu/asf/data/S1A_IW_GRDH_1SDV_20170307T120417_20170307T120442_015586_0199F9_8DCA-POEORB-30m-power-rtc-gamma.zip', 
+                 'https://hyp3-download.asf.alaska.edu/asf/data/S1A_IW_GRDH_1SDV_20170307T120442_20170307T120507_015586_0199F9_40EA-POEORB-30m-power-rtc-gamma.zip', 
+                 'https://hyp3-download.asf.alaska.edu/asf/data/S1A_IW_GRDH_1SDV_20170319T120426_20170319T120451_015761_019F2B_3A67-POEORB-30m-power-rtc-gamma.zip', 
+                 'https://hyp3-download.asf.alaska.edu/asf/data/S1A_IW_GRDH_1SDV_20170331T120426_20170331T120451_015936_01A461_2F7D-POEORB-30m-power-rtc-gamma.zip', 
+                 'https://hyp3-download.asf.alaska.edu/asf/data/S1A_IW_GRDH_1SDV_20170412T120427_20170412T120444_016111_01A9AE_156D-POEORB-30m-power-rtc-gamma.zip', 
+                 'https://hyp3-download.asf.alaska.edu/asf/data/S1A_IW_GRDH_1SDV_20170424T120428_20170424T120453_016286_01AF0F_DDC2-POEORB-30m-power-rtc-gamma.zip']
+"""
+'''
+download_urls = """
+download_urls = ['https://hyp3-download.asf.alaska.edu/asf/data/S1A_IW_GRDH_1SDV_20170223T120417_20170223T120442_015411_0194AE_F18E-POEORB-30m-power-rtc-gamma.zip', 
+                 'https://hyp3-download.asf.alaska.edu/asf/data/S1A_IW_GRDH_1SDV_20170223T120442_20170223T120507_015411_0194AE_AFD2-POEORB-30m-power-rtc-gamma.zip', 
+                 'https://hyp3-download.asf.alaska.edu/asf/data/S1A_IW_GRDH_1SDV_20170307T120417_20170307T120442_015586_0199F9_8DCA-POEORB-30m-power-rtc-gamma.zip']
+"""
+'''
+test.replace_cell("download_urls = []", download_urls)
+
+download = f'''
+product_count = 1
+for url in download_urls:
+    print(f"Product Number {{product_count}} of {{len(download_urls)}}:")
+    product_count += 1
+    product = url.split('/')[5]
+    filename = f"{{products_path}}/{{product}}"
+    # if not already present, we need to download and unzip products
+    if not os.path.exists(filename.split('.zip')[0]):
+        print(f"{{product}} is not present.")
+        print(f"Downloading from {{url}}")
+        cmd = ["wget", "-c", "-q", "--show-progress", "--http-user={username}", "--http-password={password}", url]
+        subprocess.run(cmd, capture_output=True)
+        asf_unzip(products_path, product)
+        print(f"product: {{product}}")
+        try:
+            os.remove(product)
+        except OSError:
+            pass
+'''
+test.replace_cell("cmd = get_wget_cmd(url, login)", download)
+
+test.replace_cell("coord_choice = select_parameter", "coord_choice = 'UTM'")
+
+process_type = """process_type = 2"""
+test.replace_cell("subscription_info = login.api.get_subscriptions", process_type)
+
+######## REPLACE SPECIFIC LINES IN CELLS #########
+
+polarization = "polarization = 'VV and VH'"
+test.replace_line("polarization = polarization_choice.value", "polarization = polarization_choice.value",
+                  polarization)
+
+test.replace_line("if coord_choice.value == 'UTM'", "if coord_choice.value == 'UTM':", "if coord_choice == 'UTM':")
+
+test.replace_line("if coord_choice.value == 'UTM':", "if coord_choice.value == 'UTM':", "    if coord_choice == 'UTM':")
+
+test.replace_line("elif coord_choice.value == 'Lat/Long':", "elif coord_choice.value == 'Lat/Long':", "    elif coord_choice == 'Lat/Long':")
+
+######### SKIP CELLS ###########
+
+skip_em = ["subscriptions = get_hyp3",
+           "subscription_id = subscription_id",
+           "Note: After selecting a date range",
+           "get_slider_vals(date_picker)",
+           "product_info = get_product_info",
+           "path_choice = select_mult",
+           "fp = path_choice.value",
+           "valid_directions = set()",
+           "polarizations = get_RTC_po",
+           "direction = direction_choice.value",
+           "subscription_info = login.a",
+           "polarization_choice = select_parameter",
+           ]
+for search_str in skip_em:
+    test.replace_cell(search_str)
+
+all_the_code = test.assemble_test_code()
+
+for cell_index in all_the_code:
+    print("\n-----------------------------------------------------------------")
+    print(f"Cell Index: {cell_index}")
+    print(f"Code:\n {all_the_code[cell_index]}\n")
+    print("Output:")
+    exec(all_the_code[cell_index])
+
--- asf_jupyter_test.py
+++ asf_jupyter_test.py
@@ -1,42 +1,160 @@
 # Alaska Satellite Facility
 # ASF_Jupyter_Test.py
 # Alex Lewandowski
-# 6-12-2020
+# 6-14-2020
 
 import json
 import re
 
 
-class Cell():
+def is_braced(string: str) -> bool:
+    """
+    returns: True if string starts with { and ends with }
+    """
+    regex = '({)[\S]*(})'
+    return re.search(regex, string)
+
+
+def is_filename(expression: str) -> bool:
+    regex = "^[ ]*[|A-Z|a-z|0-9|_|-]*[.][a-z]{1,6}"
+    return re.search(regex, expression) != None
+
+
+def is_format_string(code: str) -> bool:
+    f_regex = "^(f\"\"\"|f'''|f\"|f')"
+    if re.search(f_regex, code):
+        return True
+    return False
+
+
+def is_expr(expression: str) -> bool:
+    """
+    takes: a string of code
+    returns: True if looks like a function call, object reference,
+             or an indexed subscriptable object
+    """
+    regex = "^[ |A-Z|a-z|0-9|_|-]*[.|[|(]"
+    return re.search(regex, expression) != None
+
+
+def get_base_var(var: str) -> str:
+    """
+    takes: a string of code for an assignment in python,
+           with or without leading whitespaces.
+           (ex: "    variable = 'a value'")
+    returns: the variable name with no whitespaces.
+    """
+    regex = "^[ |A-z|0-9]*"
+    return (re.search(regex, var).group(0)).replace(" ", "")
+
+
+def is_f_string(code: str) -> bool:
+    f_regex = "^(f\"\"\"|f'''|f\"|f')"
+    if re.search(f_regex, code):
+        return True
+    return False
+
+
+def count_leading_whitespaces(string: str) -> int:
+    count = 0
+    for char in string:
+        if char == ' ':
+            count += 1
+        else:
+            break
+    return count
+
+
+def add_newline(code: str) -> str:
+    if code[-1] != "\n":
+        code = f"{code}\n"
+    return code
+
+
+class SearchFailedException(Exception):
+    """
+    Raised when search returns no results
+    """
+    pass
+
+
+class NotCommandLineMagicException(Exception):
+    """
+    Raised when command line magic string doesn't begin with '!'
+    """
+    pass
+
+
+class Cell:
     def __init__(self, index, cell_type, contents):
         self.index = index
         self.cell_type = cell_type
         self.contents = contents
 
 
-class SearchFailedException(Exception):
-    """Raised when search returns no results"""
-    pass
-
-
-class NotCommandLineMagicException(Exception):
-    """Raised when command line magic string doesn't begin with '!'"""
-    pass
-
-
-class OSL_Notebook_Test():
+class ASFNotebookTest:
     def __init__(self, notebook_path):
-
         self.cells = self.get_cells(notebook_path)
-
-        self.rtc_vvvh = ""
-        self.rtc_s1tbh = ""
-        self.insar = ""
-
         self.replace_cells = {}
         self.skip_cells = []
 
-    def find(self, string):
+    @staticmethod
+    def get_loop_var_names(cell_code: list) -> list:
+        """
+        takes: a list of python code strings
+        returns: a list of variable names associated with any
+                 for loops (handles enumerate)
+        """
+        var_lst = []
+        for i, line in enumerate(cell_code):
+            if "for" in line and "enumerate" not in line:
+                var_lst.append(line.split(' ')[1])
+            elif "enumerate" in line:
+                var_lst.append(line.split(' ')[1][:-1])
+                var_lst.append(line.split(' ')[1])
+        return var_lst
+
+    @staticmethod
+    def magic_calls_var_containing_cmd(code: str, test_locals: dict) -> bool:
+        """
+        Provides lookahead capability to determine if a magic ! call uses code assigned
+        to a variable in the same code cell
+
+        code: string of python code
+        test_locals: a dict of {variable names: values} assigned in the same cell as code.
+        """
+        if code[0] == '!':
+            return code[2:] in test_locals
+        else:
+            print("Error: magic_calls_var_containing_cmd() was passed magic command string missing '!'")
+            raise NotCommandLineMagicException
+
+    @staticmethod
+    def remove_dollar_magic(code_lst: list) -> list:
+        """
+        takes: a list of code strings
+        returns: the list of code strings with any leading $ removed
+        """
+        for i, chunk in enumerate(code_lst):
+            if len(chunk) > 0 and chunk[0] == '$':
+                code_lst[i] = chunk[1:]
+        return code_lst
+
+    @staticmethod
+    def subprocess_check_output_to_run(string: str) -> str:
+        """
+        takes: a string subprocess.check_output call with args
+               stderr=subprocess.PIPE and shell=True
+        returns: the call string as subprocess.run with arg capture_output=True
+        """
+        string = string.replace('check_output', 'run')
+        return string.replace("stderr=subprocess.PIPE, shell=True)", "capture_output=True)")
+
+    def find(self, string: str) -> list:
+        """
+        takes: a search string
+        returns: a list of indices of code cells containing matching strings
+        """
         results = []
         for i in self.cells:
             for line in self.cells[i].contents:
@@ -44,7 +162,11 @@ class OSL_Notebook_Test():
                     results.append(i)
         return results
 
-    def get_cells(self, notebook_path):
+    def get_cells(self, notebook_path: str):
+        """
+        takes: a path to a Jupyter Notebook
+        returns: a list of all (code) Cell objects in notebook
+        """
         doc = json.load(open(notebook_path))
         cells = dict()
         for i, c in enumerate(doc['cells']):
@@ -53,165 +175,136 @@ class OSL_Notebook_Test():
                 cells.update({i: cell})
         return cells
 
-    def replace_line(self, cell_search_str, line_search_str, replacement_code):
+    # TODO: decide how to handle duplicates. Only handle 1st and give warning?
+    def replace_line(self, cell_search_str: str, line_search_str: str, replacement_code: str):
+        """
+        cell_search_str: a search string to locate cell
+        line_search_str: a search string to locate line in cell to replace
+        replacement_code: a string of replacement code
+
+        updates self.replace_cells dictionary with altered code
+        """
         index = self.find(cell_search_str)
-
-        # TODO: decide how to handle duplicates,
-        #      only handle 1st and give warning?
-
         if len(index) > 0:
             index = index[0]
         else:
             raise SearchFailedException
         contents = self.cells[index].contents
-        replacement_code = self.add_missing_newline(replacement_code)
+        replacement_code = add_newline(replacement_code)
         for i, line in enumerate(contents):
             if line_search_str in line:
                 contents[i] = replacement_code
         self.replace_cells.update({index: contents})
 
+    # TODO: decide how to handle duplicates. Only handle 1st and give warning?
     def replace_cell(self, search_str, replacement_code=""):
+        """
+        search_str: a search string to locate cell
+        replacement_code: a string of replacement cell code
+
+        updates self.replace_cells dictionary with altered code
+        """
         index = self.find(search_str)
-
-        # TODO: decide how to handle duplicates,
-        #      only handle 1st and give warning?
-
         if len(index) > 0:
             index = index[0]
         else:
             raise SearchFailedException
-
         self.replace_cells.update({index: [replacement_code]})
 
-    def skip_cell(self, search_str):
-        index = self.find(search_str)
+    def convert_to_subprocess(self, code: str, assignments: dict, loop_vars: list) -> str:
+        """
+        code: a string of a ! Jupyter Notebook magic command
+        assignments: a dict containing all assigned variable names and values in code cell
+        loop_vars: a list of all loop variable names in code cell
 
-        # TODO: decide how to handle duplicates,
-        #      only handle 1st and give warning?
-
-        if len(index) > 0:
-            index = index[0]
-        else:
-            raise SearchFailedException
-        self.skip_cells.append(index)
-
-    def add_missing_newline(self, code: str) -> str:
-        if code[-1] != "\n":
-            code = f"{code}\n"
-        return code
-
-    def remove_dollar_magic(self, code):
+        returns: a string containing the ! command converted into subprocess.check_output
+                 and subprocess.run commands, which when called, tries to call 'check_output' first,
+                 and then 'run' if it encounters subprocess.CalledProcessError
+        """
+        arg_lst = []
+        args = None
+        spaces = count_leading_whitespaces(code)
+        code = code[spaces + 1:]  # drop the !
         code_lst = code.split(' ')
-        for i, chunk in enumerate(code_lst):
-            if len(chunk) > 0 and chunk[0] == '$':
-                code_lst[i] = chunk[1:]
-        return code_lst
+        code_lst = self.remove_dollar_magic(code_lst)
 
-    def count_leading_whitespaces(self, string):
-        count = 0
-        for char in string:
-            if char == ' ':
-                count += 1
-            else:
-                break
-        return count
-
-    def magic_calls_var_containing_cmd(self, code: str, test_locals: list):
-        if code[0] == '!':
-            return code[2:] in test_locals
-        else:
-            print("Error: magic_calls_var_containing_cmd() was passed magic command string missing '!'")
-            raise NotCommandLineMagicException
-
-    def is_f_string(self, code: str) -> bool:
-        f_regex = "^(f\"\"\"|f'''|f\"|f')"
-        if re.search(f_regex, code):
-            return True
-        return False
-
-    def format_arg_list_f_str(self, args: list) -> list:
-        for i, _ in enumerate(args):
-            if len(args[i]) > 1:
-                if args[i][:2] == 'f"':
-                    args[i] = args[i][2:]
-                if args[i][0] == '{' and args[i][-1] == '}':
-                    args[i] = args[i][1:-1]
-                if len(args[i]) > 3:
-                    if args[i][0] == '{' and args[i][-3] == '}':
-                        args[i] = args[i][1:-3]
-                if args[i][-2:] == '"\n':
-                    args[i] = args[i][:-2]
-        return args
-
-    def is_format_string(self, code: str) -> bool:
-        f_regex = "^(f\"\"\"|f'''|f\"|f')"
-        if re.search(f_regex, code):
-            return True
-        return False
-
-    # TODO: Write it
-    def format_arg_list_format_str(self, args: list) -> list:
-        print(args)
-        return args
-
-    # FIXME: subprocess.check_output won't work for all calls
-    def convert_to_subprocess(self, code: str, assignments: dict, spaces: int):
-        arg_lst = None
-        code_lst = self.remove_dollar_magic(code[1:])
-        for i, chunk in enumerate(code_lst):
-            if len(chunk) > 0 and chunk[-1] == '\n':
-                chunk = chunk[:-1]
-            if chunk[1:] in assignments:
-                args = assignments[chunk[1:]]
-            elif chunk[1:] in locals():
-                args = locals()[chunk[1:]]
-            else:
-                arg_lst = f'["{chunk[1:]}"]'
+        for i, exp in enumerate(code_lst):
+            if exp == "":
+                continue
+            if len(exp) > 0 and exp[-1] == '\n':
+                code_lst[i] = exp[:-1]
+            if code_lst[i] in assignments:
+                code_lst[i] = assignments[code_lst[i]]
+            elif code_lst[i] in locals():
+                code_lst[i] = locals()[code_lst[i]]
         spaces = " " * spaces
-        if not arg_lst:
-            args = args.split(' ')
-        if self.is_f_string(args):
-            arg_lst = self.format_arg_list_f_str(arg_lst)
-        elif self.is_format_string(args):
-            arg_lst = self.format_arg_list_format_str(arg_lst)
-        subprocess_cmd = f"{spaces}process = subprocess.check_output({arg_lst}, stderr=subprocess.PIPE, shell=True)"
-        prnt = f"\n{spaces}print(process.decode(\"utf-8\"))\n"
-        subprocess_cmd = f"""{subprocess_cmd}{prnt}"""
-        # print(f"subprocess_cmd:\n{subprocess_cmd}")  ###### DEBUGGING
+        subprocess_cmd = f"process = subprocess.check_output(["
+        if is_f_string(code_lst[0]):
+            args = code_lst[0].split(' ')
+            args[0] = args[0][2:]  # drop the f"
+            args[len(args) - 1] = args[len(args) - 1][:-1]  # drop the "
+            for i, arg in enumerate(args):
+                if is_braced(arg):
+                    args[i] = f'f"{args[i]}"'
+            code_lst = args
+        for i, exp in enumerate(code_lst):
+            if exp in loop_vars or (is_expr(exp) and not is_filename(exp)) or is_f_string(exp):
+                if i == 0:
+                    subprocess_cmd = f"{subprocess_cmd}{exp}"
+                else:
+                    subprocess_cmd = f"{subprocess_cmd}, {exp}"
+            else:
+                if i == 0:
+                    subprocess_cmd = f"{subprocess_cmd}'{exp}'"
+                else:
+                    subprocess_cmd = f"{subprocess_cmd}, '{exp}'"
+        subprocess_cmd = f"{subprocess_cmd}], stderr=subprocess.PIPE, shell=True)"
+        prnt = f"print(process.decode(\"utf-8\"))"
+        sub_run_cmd = f"{self.subprocess_check_output_to_run(subprocess_cmd)}"
+        subprocess_cmd = f"""{spaces}{f"try:"}\n{spaces}    {subprocess_cmd}\n{spaces}    {prnt}
+{spaces}{f"except subprocess.CalledProcessError:"}\n{spaces}    {f"try:"}
+{spaces}        {sub_run_cmd}\n{spaces}    {f"except subprocess.CalledProcessError:"}
+{spaces}        {f"raise"}\n"""
         return subprocess_cmd
 
-    def get_base_var(self, var: str):
-        regex = "^( |)*\w*"
-        return (re.search(regex, var).group(0)).replace(" ", "")
-
     # FIXME: handle all jupyter notebook magic commands
-    def prepare_code_cell(self, code):
+    def prepare_code_cell(self, code: list) -> str:
+        """
+        takes: a list of code strings in a code cell
+        returns: a string of code-cell code, ready to run, with
+                 Jupyter Notebook magic commands handled
+        """
+        loop_vars = self.get_loop_var_names(code)
         assignments = {}
         code_block = """"""
         for c in code:
             if ('%%' in c and "\"" not in c) or \
                     "clear_output()" in c:
                 continue
-            assignment_regex = "^[ |A-z]*(=| =)[^=]"
+            assignment_regex = "^[ |A-z|0-9]*(=| =)[^=]"
             assignment = re.search(assignment_regex, c)
             if assignment:
-                var_regex = "^[ |]*[A-z]*"
+                var_regex = "^[ |]*[A-z|0-9]*"
                 var = re.search(var_regex, assignment.group(0))
                 var = var.group(0).replace(' ', '', 1)
-                base_var = self.get_base_var(var)
+                base_var = get_base_var(var)
                 val = c.replace(var, '')
                 space_equals_regex = "^[ |]*[=][| ]*"
                 val = val.replace(re.search(space_equals_regex, val).group(0), '')
-                # print(f"BASE_VAR: '{base_var}'")  ########### DEBUG
-                # print(f"VAL: '{val}'")            ########### DEBUG
+                if val[-1] == '\n':
+                    val = val[:-1]
                 assignments.update({base_var: val})
-            spaces = self.count_leading_whitespaces(c)
+            spaces = count_leading_whitespaces(c)
             if len(c) > 0 and c[spaces] == '!':
-                c = self.convert_to_subprocess(c, assignments, spaces)
+                c = self.convert_to_subprocess(c, assignments, loop_vars)
             code_block = f"""{code_block}{c}"""
         return code_block
 
-    def assemble_test_code(self, stop_cell=None):
+    def assemble_test_code(self, stop_cell=None) -> dict:
+        """
+        takes: an optional code cell index at which to stop
+        returns: a dictionary {code-cell index: prepared code-cell string}
+        """
         print("Assembling Notebook Test Code...")
         all_the_code = {-1: "import subprocess"}
         for index in self.cells:
@@ -225,4 +318,3 @@ class OSL_Notebook_Test():
                 code = self.prepare_code_cell(code)
                 all_the_code.update({index: code})
         return all_the_code
-
